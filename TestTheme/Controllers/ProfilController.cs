cdf90458b3194b9f64674112fe25212e808da473
aliceyhunataykan <ceyhunataykan@gmail.com>
2019-11-19 20:03:47 +0300

    Yeni Düzenlemeler Eklendi

---
+++ TestTheme/Controllers/ProfilController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestTheme.Controllers
+{
+    public class ProfilController : Controller
+    {
+        // GET: Profil
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FotoGuncelle()
+        {
+            return View();
+        }
+        public ActionResult ProfilDuzenle()
+        {
+            return View();
+        }
+        public ActionResult EpostaDegistir()
+        {
+            return View();
+        }
+        public ActionResult ParolaDegistir()
+        {
+            return View();
+        }
+        public ActionResult TwoFactor()
+        {
+            return View();
+        }
+        public ActionResult UyelikIptal()
+        {
+            return View();
+        }
+    }
+}
