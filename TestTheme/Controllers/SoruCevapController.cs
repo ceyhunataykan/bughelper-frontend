9ad5946ec0f95c7278661fad1a5de11f3a995f1e
aliceyhunataykan <ceyhunataykan@gmail.com>
2019-11-24 03:58:58 +0300

    ckeditor ile soru yazma kismi eklendi, soru gösterim formati degisti

---
+++ TestTheme/Controllers/SoruCevapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestTheme.Controllers
+{
+    public class SoruCevapController : Controller
+    {
+        // GET: SoruCevap
+        public ActionResult SoruYaz()
+        {
+            return View();
+        }
+    }
+}
