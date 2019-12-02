74fde691086d7e62ebb30472fdda93441f04913e
aliceyhunataykan <ceyhunataykan@gmail.com>
2019-12-02 12:23:45 +0300

    Uyelik iptali sayfasi eklendi, bazi duzenlemeler yapildi

--- TestTheme/Controllers/SoruCevapController.cs
+++ TestTheme/Controllers/SoruCevapController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestTheme.Models;
 
 namespace TestTheme.Controllers
 {
     public class SoruCevapController : Controller
     {
-        // GET: SoruCevap
+        [HttpGet]
         public ActionResult SoruYaz()
         {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SoruYaz(Sorular model)
+        {
+            var gelenicerik = model.SoruIcerik;
+            ViewBag.message = gelenicerik.ToString();
             return View();
         }
     }
